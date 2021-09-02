bc91202487adfdb3d1d682cb588b2dedea3d8d94
konoha <konohayuya1222@gmail.com>
2021-09-03 04:00:20 +0900

    fix can not install exa
    いまのubuntuだとインストールできなかったので修正

--- etc/ubuntu/init.sh
+++ etc/ubuntu/init.sh
@@ -2,7 +2,17 @@
 # apt update etc..
 sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y  && sudo apt autoremove -y
 
-sudo apt install -y curl vim fish httpie exa rust-bat hexyl
+sudo apt install -y curl vim fish httpie bat hexyl
+
+curl -fLo exa.zip \
+  "https://github.com/ogham/exa/releases/download/v0.10.0/exa-linux-x86_64-v0.10.0.zip" && \
+  unzip -d ./exa exa.zip && \
+  sudo mv ./exa/bin/exa /usr/local/bin/ && \
+  sudo mv ./exa/man/exa.1 /usr/share/man/man1 && \
+  sudo mv ./exa/man/exa_colors.5 /usr/share/man/man5 && \
+  sudo mv ./exa/completions/exa.fish /usr/share/fish/vendor_completions.d/ && \
+  rm -rf ./exa exa.zip
+
 
 curl -fLo powerline-go \
   "https://github.com/justjanne/powerline-go/releases/download/v1.21.0/powerline-go-linux-amd64" && \
