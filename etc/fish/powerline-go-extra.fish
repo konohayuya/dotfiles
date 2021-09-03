de0e1840f0f5f42602326c69b900be2db8febaf8
konoha <konohayuya1222@gmail.com>
2021-09-03 09:53:48 +0900

    fix to apply fish colors and add extra env

---
+++ etc/fish/powerline-go-extra.fish
@@ -0,0 +1,1 @@
+set PROMPT_USER -U \x5cxE2\x5cx9C\x5cxA8
--- etc/ubuntu/deploy.sh
+++ etc/ubuntu/deploy.sh
@@ -6,7 +6,7 @@ ln -sfn ${DOTFILES_DIR}/.config/fish/functions/la.fish ~/.config/fish/functions/
 ln -sfn ${DOTFILES_DIR}/.config/fish/functions/u.fish ~/.config/fish/functions/
 ln -sfn ${DOTFILES_DIR}/.config/fish/functions/uu.fish ~/.config/fish/functions/
 
-fish ${DOTFILES_DIR}/etc/fish/color.fish
+fish ${DOTFILES_DIR}/etc/fish/*
 
 # httpie
 mkdir -p ~/.httpie
