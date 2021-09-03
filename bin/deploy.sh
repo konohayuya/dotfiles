afc43f535d3beb107555714313d8f8ad723f8d45
konoha <konohayuya1222@gmail.com>
2021-09-03 11:47:01 +0900

    support for wsl ubuntu

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,6 +6,15 @@ if echo "$UNAME" | grep -q 'Darwin'; then
   bash ${DOTFILES_DIR}/etc/osx/deploy.sh
 elif echo "$UNAME" | grep -q 'Ubuntu'; then
   bash ${DOTFILES_DIR}/etc/ubuntu/deploy.sh
+elif echo "$UNAME" | grep -q 'microsoft'; then
+  # wsl
+  if grep '^NAME="Ubuntu' '/etc/os-release' >/dev/null; then
+    # wsl ubuntu
+    bash ${DOTFILES_DIR}/etc/ubuntu/deploy.sh
+  else
+    echo 'unsupporting'
+    exit 1
+  fi
 else
   echo 'unsupporting'
   exit 1
--- bin/init.sh
+++ bin/init.sh
@@ -6,6 +6,15 @@ if echo "$UNAME" | grep -q 'Darwin'; then
   bash ${DOTFILES_DIR}/etc/osx/init.sh
 elif echo "$UNAME" | grep -q 'Ubuntu'; then
   bash ${DOTFILES_DIR}/etc/ubuntu/init.sh
+elif echo "$UNAME" | grep -q 'microsoft'; then
+  # wsl
+  if grep '^NAME="Ubuntu' '/etc/os-release' >/dev/null; then
+    # wsl ubuntu
+    bash ${DOTFILES_DIR}/etc/ubuntu/init.sh
+  else
+    echo 'unsupporting'
+    exit 1
+  fi
 else
   echo 'unsupporting'
   exit 1
