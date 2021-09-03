04cb076cf8bfb056ef7dd3da887062720a93a8ac
konoha <konohayuya1222@gmail.com>
2021-09-03 10:33:52 +0900

    avoid unnecessary installation of brew

--- etc/osx/init.sh
+++ etc/osx/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 # install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+type 'brew' 2>&1 >/dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install curl vim fish powerline-go httpie exa bat
 
