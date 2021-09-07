2141aa5f516524b94de2ea356764c9729da8662b
konoha <konohayuya1222@gmail.com>
2021-09-08 02:26:39 +0900

    update setup.sh for option to only deploy mode
    deploy単体だけで実行可能にした

--- setup.sh
+++ setup.sh
@@ -2,7 +2,16 @@
 
 export DOTFILES_DIR=$(cd $(dirname $0); pwd)
 
-bash ${DOTFILES_DIR}/bin/init.sh
-bash ${DOTFILES_DIR}/bin/deploy.sh
+while getopts 'di' flag; do
+  case "${flag}" in
+    d) bash ${DOTFILES_DIR}/bin/deploy.sh
+      ;;
+    i) bash ${DOTFILES_DIR}/bin/init.sh
+      ;;
+    *) bash ${DOTFILES_DIR}/bin/init.sh
+      bash ${DOTFILES_DIR}/bin/deploy.sh
+      ;;
+  esac
+done
 
 unset DOTFILES_DIR
